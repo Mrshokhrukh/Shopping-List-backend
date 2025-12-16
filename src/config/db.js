ad07cad212a222e8a528d3ff50cf8f6ee40b2532
Mrshokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2025-12-16 23:08:52 +0500

    db changes

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect('mongodb+srv://shokhrukh:2wkO7GydrXAHaZYm@cluster0.8zzlwps.mongodb.net/groups')
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`)
+    console.log(`✅ MongoDB Connected`)
   } catch (error) {
     console.error(`❌ Error: ${error.message}`)
     process.exit(1)
