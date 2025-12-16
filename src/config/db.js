1d599ce4671b75efefa19715db48f7fb35cd2870
Mrshokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2025-12-16 23:16:04 +0500

    connect db

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect('mongodb+srv://shokhrukh:2wkO7GydrXAHaZYm@cluster0.8zzlwps.mongodb.net/groups')
-    console.log(`✅ MongoDB Connected`)
+    console.log(`MongoDB Connected`)
   } catch (error) {
-    console.error(`❌ Error: ${error.message}`)
+    console.error(`Error: ${error.message}`)
     process.exit(1)
   }
 }
